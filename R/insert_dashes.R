8c51cba23774d6cd3d1328c6d1fd852d12733056
Mine Cetinkaya-Rundel <mine@stat.duke.edu>
2018-08-13 00:25:12 -0400

    Ok, the verdict is no, you don't need to provide location and id to insertText

--- R/insert_dashes.R
+++ R/insert_dashes.R
@@ -9,24 +9,13 @@ insert_dashes <- function(){
   # Get active document context
   context <- rstudioapi::getActiveDocumentContext()
 
-  # Store document id
-  context_id <- context$id
-
-  # Get cursor row and column
-  current_row <- context$selection[[1]]$range[[1]][[1]]
+  # Get column of cursor position
   current_col <- context$selection[[1]]$range[[1]][[2]]
 
   # Calculate how many dashes from current cursor position until column 80
   num_dashes <- 80 - current_col + 1
 
-  # Store where to start inserting dashes
-  insert_pos <- c(current_row, current_col + 1)
-
   # Insert dashes
-  rstudioapi::insertText(
-    #location = insert_pos,
-    text     = paste0(strrep("-", num_dashes), "\n")
-    #id       = context_id
-    )
+  rstudioapi::insertText(paste0(strrep("-", num_dashes), "\n"))
 
 }
