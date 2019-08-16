a2e0155e42a2277cb171df6855db9a6d3b821bab
Mine Cetinkaya-Rundel <mine@stat.duke.edu>
2019-08-16 22:54:40 +0100

    Add new addin: launch empty RStudio session

---
+++ R/launch_rstudio.R
@@ -0,0 +1,12 @@
+#' Launch a new project-less RStudio session
+#'
+#' Call this function as an addin to launch a new project-less RStudio session
+#' without closing your currently open RStudio project(s).
+#'
+#' @export
+
+launch_rstudio <- function(){
+
+  rstudioapi::terminalExecute("open -n /Applications/RStudio.app", show = F)
+
+}
