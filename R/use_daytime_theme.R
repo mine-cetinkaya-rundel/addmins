2e15dbb56a54914258a87fe273cfef1d82c5b2b3
Mine Çetinkaya-Rundel <cetinkaya.mine@gmail.com>
2020-01-23 09:38:03 +0000

    Quickly switch between day and night time with addins

---
+++ R/use_daytime_theme.R
@@ -0,0 +1,12 @@
+#' Use daytime theme.
+#'
+#' Quickly switch to daytime theme, currently this is Solarized Light.
+#'
+#' @export
+
+ude_daytime_theme <- function(){
+
+  # Set default theme
+  rstudioapi::applyTheme("Solarized Light")
+
+}
---
+++ R/use_nighttime_theme.R
@@ -0,0 +1,12 @@
+#' Use nighttime theme.
+#'
+#' Quickly switch to nighttime theme, currently this is Solarized Dark.
+#'
+#' @export
+
+ude_daytime_theme <- function(){
+
+  # Set default theme
+  rstudioapi::applyTheme("Solarized Dark")
+
+}
